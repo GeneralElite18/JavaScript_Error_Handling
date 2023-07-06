3791361dfb9ad562d2cfe8a46f5983d1a7419c27
Alex Gonzales <gonzales.alexzander18@gmail.com>
2023-07-06 13:14:19 -0600

    finished exercise

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 // TODO: Enable strict mode
+"use strict";
 
 // TODO: Fix the following parameter list
-function parseToJSON(data, data) {
+function parseToJSON(data) {
   // TODO: Add a try/catch block to
+  try {
+    return JSON.parse(data); 
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
   // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
+  
   
   // TODO: if an exception is raised
   // print the error to the console
